273a7fc79b5d93e687b5aa56abcc38d098f2e0f9
anieo <ali.maher.1460@gmail.com>
2020-04-23 13:52:35 +0200

    update script and wsgi

---
+++ flaskapp.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, '/var/www/html/flask-blog')
+from flaskblog import app as application
--- flaskblog.py
+++ flaskblog.py
@@ -53,4 +53,5 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run()
+    # host='0.0.0.0',debug=True
