e9e1c3c828e35a002a581970038dc62ee453a1b0
anieo <ali.maher.1460@gmail.com>
2020-04-24 16:46:33 +0200

    final update?

--- flaskblog.wsgi
+++
@@ -1,9 +0,0 @@
-activate_this = '/var/www/html/flask-blog/venv/bin/activate'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
-import sys
-sys.path.insert(0, '/var/www/html/flask-blog/')
-from flaskblog import app as application
-
-
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+sudo apt-get update  -y
+sudo apt install python3.6 -y
+sudo apt install python3-pip -y
+sudo apt install apache2 -y
+sudo apt install libapache2-mod-wsgi -y
+
+sudo rm /usr/bin/python
+sudo ln-n /usr/bin/python3.6 /usr/bin/python
+
+sudo apt install virtualenv -y
+sudo systemctl enable apache2.service
+sudo systemctl start apache2.service
+
+sudo usermod -a -G ubuntu
+sudo chown -R ubuntu:ubuntu /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+
+sudo apt install git -y
+sudo git clone https://github.com/anieo/flask-blog /var/www/html/flask-blog
+sudp cp  /var/www/html/flask-blog/flask.conf /etc/apache2/sites-available/flask.conf
+sudo a2ensite flask.conf
+virtualenv -p /usr/bin/python3.6 /var/www/html/flask-blog/venv
+source /var/www/html/flask-blog/venv/bin/activate
+cd  /var/www/html/flask-blog/
+pip install -r requirements.txt
+
+
+pip3 install -U flask email_validator Flask-WTF
+
+sudo systemctl restart apache2.service
