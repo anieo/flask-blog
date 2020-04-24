b7b5ffaaac5d35bc3dbc1457f2ecbfcb27a1a0e6
anieo <ali.maher.1460@gmail.com>
2020-04-24 17:24:33 +0200

    small update

--- install.sh
+++ install.sh
@@ -18,17 +18,15 @@ sudo chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+pip3 install -U flask email_validator Flask-WTF
 
 sudo apt install git -y
 sudo git clone https://github.com/anieo/flask-blog /var/www/html/flask-blog
-sudp cp  /var/www/html/flask-blog/flask.conf /etc/apache2/sites-available/flask.conf
+sudo cp  /var/www/html/flask-blog/flask.conf /etc/apache2/sites-available/flask.conf
 sudo a2ensite flask.conf
-virtualenv -p /usr/bin/python3.6 /var/www/html/flask-blog/venv
+virtualenv  /var/www/html/flask-blog/venv
 source /var/www/html/flask-blog/venv/bin/activate
 cd  /var/www/html/flask-blog/
 pip install -r requirements.txt
-
-
-pip3 install -U flask email_validator Flask-WTF
 
 sudo systemctl restart apache2.service
